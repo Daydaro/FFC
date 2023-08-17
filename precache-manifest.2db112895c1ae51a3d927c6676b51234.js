b0c46bd329937ecd9a5e113ba55f47af2c2124ae
Dani Bostina <danibostina94@gmail.com>
2023-08-18 00:11:40 +0300

    Updates

---
+++ precache-manifest.2db112895c1ae51a3d927c6676b51234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9cd2c0c7a2367b45ede",
+    "url": "/Daydaro/FFC/static/css/main.f19efcd0.chunk.css"
+  },
+  {
+    "revision": "d9cd2c0c7a2367b45ede",
+    "url": "/Daydaro/FFC/static/js/main.d9cd2c0c.chunk.js"
+  },
+  {
+    "revision": "bd78a5334beda9fcdcb7",
+    "url": "/Daydaro/FFC/static/js/1.bd78a533.chunk.js"
+  },
+  {
+    "revision": "23ca2313856020c2a1ac",
+    "url": "/Daydaro/FFC/static/js/2.23ca2313.chunk.js"
+  },
+  {
+    "revision": "5918ea242e016387930f",
+    "url": "/Daydaro/FFC/static/js/runtime~main.5918ea24.js"
+  },
+  {
+    "revision": "b7dcb34ea5bb52eb43750497ed175a7e",
+    "url": "/Daydaro/FFC/index.html"
+  }
+];
